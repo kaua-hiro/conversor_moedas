85a2529c73b277a455957f0afb8ef89c6670cfb6
Kauã Hiro <kauahiro2004@hotmail.com>
2026-05-04 09:37:09 -0300

    feat: implementa logica de conversao, validacoes e formatacao de moeda

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -44,6 +44,7 @@ export default function App() {
     }).format(value);
   };
 
+  // Lógica principal de conversão e formatação matemática
   const handleConvert = () => {
     Keyboard.dismiss();
 
